378f2f10691b610ec6bd29795db48f0e347844f3
joakim <joakim.askenback@gmail.com>
2015-01-08 19:29:10 +0100

    LD31 - Lolfest - implement minifyer of js

--- ld31.js
+++ ld31.js
@@ -12,6 +12,13 @@ var genCookie = function () {
 
 // Set up app with Express framework
 var app = express();
+//app.set('env', 'production');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+if (app.get('env') === 'development') {
+    app.set('view cache', false);
+}
 
 // Create server
 var server = app.listen(process.env.PORT || 3300, function() {
@@ -37,7 +44,57 @@ app.use(function (req, res, next) {
     next(); // <-- important!
 });
 
-app.use('/', express.static('public/'));
+var assets = {
+    "lib.js": {
+        type: "js",
+        dir: "js",
+        files: [
+            "../lib/underscore-min.js",
+            "../lib/socket.io-1.2.1.js",
+            "../lib/pixi.js"
+        ]
+    },
+    "game.js" : {
+        type: "requirejs",
+        dir: "js",
+        main: "game.js",
+        lib: "../lib/require.js",
+        includeLib: false
+    },
+    "style.css" : {
+        type: "css",
+        dir: "css",
+        files: [
+            "style.css"
+        ]
+    }
+};
+
+var assetManagerConfig = {
+    rootRoute   : "/static",
+    srcDir      : "./public",
+    buildDir    : "./builtAssets",
+    process     : true,
+    env         : app.get("env")
+};
+
+app.use(require("express-asset-manager")(assets, assetManagerConfig, function () {
+    console.log('callback called', arguments);
+}));
+
+if (app.get("env") === "development") {
+    console.log('development mode');
+    app.use('/static', express.static('public'));
+} else {
+    console.log('production mode');
+    // in production, use a reverse proxy instead
+    app.use('/static', express.static('builtAssets'));
+}
+app.use('/', express.static('public'));
+app.use('/', function (req, res) {
+    res.render('index');
+});
+
 app.use('/webhook', hookshot('refs/heads/master', 'git pull'));
 
 // Connect DB
@@ -61,26 +118,19 @@ db.once('open', function callback () {
 // Tell Socket.io to pay attention
 io = io.listen(server, { log: false });
 
-// Sockets object to save game code -> socked associations
-var socketCodes = {};
-
 // When a client connects...
 io.sockets.on('connection', function(socket) {
     // Confirm the connection
     socket.emit('welcome', {});
 
-    socket.on('message', function (data) {
-    });
+    socket.on('message', function (data) {});
 
     // send start command to game client
-    socket.on('start', function(data) {
-    });
+    socket.on('start', function(data) {});
 
     // send disconnect command to game client
-    socket.on('disconnect', function(data) {
-    });
+    socket.on('disconnect', function(data) {});
 });
 
 // When a client disconnects...
-io.sockets.on('disconnect', function(socket) {
-});
+io.sockets.on('disconnect', function(socket) {});
---
+++ public/js/Animation.js
@@ -0,0 +1,98 @@
+var Animation = function (settings) {
+    this.settings = settings || {};
+    this.name = this.settings.name;
+    this.type = this.settings.type || Animation.types.LINEAR;
+    this.frames = this.settings.frames;
+    updateFuncs.push(proxy(this.update, this));
+};
+
+Animation.types = {
+    LINEAR: 'linear',
+    PING_PONG: 'pingpong',
+    LOOP: 'loop'
+};
+
+Animation.prototype = {
+    name: '',
+    type: null,
+    animationSpeed: 1,
+    animationDirection: 1,
+    frames: [],
+    currentFrameIndex: 0,
+    elapsed: 0,
+    playing: false,
+    paused: false,
+    getFrame: function () {
+        if (this.currentFrameIndex >= 0 && this.currentFrameIndex < this.frames.length) {
+            return this.frames[this.currentFrameIndex];
+        }
+        return _.extend({}, Frame.prototype);
+    },
+    getSprite: function () {
+        return this.getFrame().sprite;
+    },
+    atEnd: function () {
+        if (this.type === Animation.types.PING_PONG) {
+            if (this.animationDirection > 0) {
+                return this.currentFrameIndex >= this.frames.length;
+            } else {
+                return this.currentFrameIndex == 0;
+            }
+        }
+
+        return this.currentFrameIndex >= this.frames.length;
+    },
+    setNext: function () {
+        this.parent.root.removeChild(this.getSprite());
+        this.currentFrameIndex += this.animationDirection;
+        if (this.atEnd()) {
+            if (this.type === Animation.types.PING_PONG) { // change direction
+                this.animationDirection *= -1;
+                if (this.currentFrameIndex === this.frames.length) {
+                    this.currentFrameIndex += this.animationDirection * 2;
+                }
+            } else if (this.type === Animation.types.LOOP) { // loop
+                this.currentFrameIndex = 0;
+            } else { // LINEAR
+                this.currentFrameIndex = this.frames.length - 1; // stop at end
+            }
+        }
+        this.parent.root.addChild(this.getSprite());
+    },
+    stop: function () {
+        if (this.playing) {
+            this.playing = false;
+            this.paused = false;
+            this.elapsed = 0;
+            this.parent.root.removeChild(this.getSprite());
+        }
+        return this;
+    },
+    pause: function () {
+        this.playing = false;
+        this.paused = true;
+        return this;
+    },
+    start: function () {
+        if (!this.playing) {
+            this.playing = true;
+            if (!this.paused) {
+                this.elapsed = 0;
+            }
+            this.paused = false;
+            this.parent.root.addChild(this.getSprite());
+        }
+        return this;
+    },
+    update: function (delta, now) {
+        if (this.playing) {
+            this.elapsed += delta * this.animationSpeed;
+            while (this.elapsed > this.getFrame().duration) {
+                this.elapsed -= this.getFrame().duration;
+                this.setNext();
+            }
+        }
+    }
+}
+
+module.export = Animation;
